ed2bcd5cec4ae7d34a04438a19ecd20ecfcb52aa
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-04-01 13:44:37 +0000

    commited circle_area.py file

---
+++ Basics/circle_area.py
@@ -0,0 +1,10 @@
+import math
+
+
+def computeCircleArea(radius: int) -> float:
+    pi = math.pi
+    area = pi*(radius)**2
+    print(area)
+
+
+computeCircleArea(1.1)
--- Basics/current_time.py
+++ Basics/current_time.py
@@ -6,7 +6,6 @@ def getCurrentTime():
     local_time = time.localtime(seconds)
     time_str = time.strftime('%Y-%m-%d, %H:%M:%S', local_time)
     print(time_str)
-    # return local_time
 
 
 getCurrentTime()
