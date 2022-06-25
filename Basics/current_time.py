21ef3fc5d1906acc560ccb78dfca43e5a0d34657
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-06-25 17:32:34 +0000

    get current time

---
+++ Basics/current_time.py
@@ -0,0 +1,11 @@
+import time
+
+
+def getCurrentTime():
+    seconds = time.time()
+    local_time = time.localtime(seconds)
+    time_str = time.strftime('%Y-%m-%d, %H:%M:%S', local_time)
+    print(time_str)
+
+
+getCurrentTime()
