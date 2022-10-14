2e3eb837829d60fc1a65ca56fc8b27e4752b7fa6
sadatyussuf <tommyleespartan0@gmail.com>
2022-10-14 20:02:23 +0000

    created the main file

--- intermediate/convert_json_to_csv/main.py
+++ intermediate/convert_json_to_csv/main.py
@@ -1,4 +1,15 @@
+from json_to_csv import read_file
+from json_to_csv.convert_json import write_to_csv
+
+
+def main(filename):
+    data = read_file(filename)
+    write_to_csv(data)
 
 
 if __name__ == '__main__':
-    print(__name__)
+    main('data.json')
+    # data = read_file('data.json')
+    # write_to_csv(data)
+
+    # print(data)
