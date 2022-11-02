a29e87db3434a23fdd04eaf30b23b39025e44da9
sadatyussuf <tommyleespartan0@gmail.com>
2022-11-02 19:09:53 +0000

    instantiated the LightNovelScrapper class and called all the methods

--- intermediate/lightnovel_scrapper/main.py
+++ intermediate/lightnovel_scrapper/main.py
@@ -1,5 +1,9 @@
 from lightnovel import LightNovelScrapper
 from rich import print as rprint
 
-test = LightNovelScrapper()
-test.test_url()
+# test = LightNovelScrapper()
+# test.test_url()
+with LightNovelScrapper() as bot:
+    bot.landing_page()
+    bot.select_genre('action')
+    bot.refresh()
