ce1e8280ccb5496eefc5725bf0aeffcab9140003
sadatyussuf <tommyleespartan0@gmail.com>
2022-11-02 19:08:23 +0000

    added Exit dunder method, get_landing_page method and select_genre method

--- intermediate/lightnovel_scrapper/lightnovel/lightnovel_search.py
+++ intermediate/lightnovel_scrapper/lightnovel/lightnovel_search.py
@@ -1,24 +1,49 @@
 from selenium import webdriver
 from rich import print as rprint
 import os
+from lightnovel import constants as const
 
 
 class LightNovelScrapper(webdriver.Chrome):
-    def __init__(self, driver=r'C:\SeleniumDrivers'):
+    def __init__(self, driver=r'C:\SeleniumDrivers',close=False):
         self.driver = driver
+        self.close = close
 
         os.environ['PATH'] += self.driver
         super(LightNovelScrapper, self).__init__()
         self.implicitly_wait(30)
 
-    def test_url(self):
-        self.get('http://demo.seleniumeasy.com/jquery-download-progress-bar-demo.html')
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.close:
+            self.quit()
 
-        btn = self.find_element(
+    def landing_page(self):
+        self.get(const.BASE_URL)
+
+    def select_genre(self,genre):
+        genre_btn = self.find_element(
             by='css selector',
-            value= '#downloadButton'
+            # value='#hot-genre-select'
+            value='.list-genre .row'
         )
-        btn.click()
+        # genre_btn.click()
+
+        genre_options = genre_btn.find_elements(
+            by='css selector',
+            # value= 'option'
+            value='div'
+        )
+        for option in genre_options:
+            option_text = option.find_element(
+                by='css selector',
+                value='a'
+            ).get_attribute('innerHTML').strip().lower()
+            if option_text == genre:
+                option.click()
+                # break
+
+
+
 
 
 
