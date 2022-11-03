48aa5e42773161616a973283fc2307f6f67725da
sadatyussuf <tommyleespartan0@gmail.com>
2022-11-03 21:49:02 +0000

    called collect_results method

--- intermediate/lightnovel_scrapper/main.py
+++ intermediate/lightnovel_scrapper/main.py
@@ -1,9 +1,7 @@
 from lightnovel import LightNovelScrapper
-from rich import print as rprint
 
-# test = LightNovelScrapper()
-# test.test_url()
 with LightNovelScrapper() as bot:
     bot.landing_page()
     bot.select_genre('action')
     bot.refresh()
+    bot.collect_results(20)
