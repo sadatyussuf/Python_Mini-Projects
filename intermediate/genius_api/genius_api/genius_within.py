09414ac5a6f59d818e345dc2d4c007fbe90b6ca4
sadatyussuf <tommyleespartan0@gmail.com>
2022-11-09 09:15:01 +0000

    still working on it

--- intermediate/genius_api/genius_api/genius_within.py
+++ intermediate/genius_api/genius_api/genius_within.py
@@ -7,20 +7,23 @@ load_dotenv()
 token = os.environ.get('GENIUS_ACCESS_TOKEN')
 
 
-class TheGeniusWithin(Genius):
-    def __init__(self, client_token=token, close=False):
-        self.client_token = client_token
-        self.close = close
-        print(token)
+class TheGeniusWithin:
+    def __init__(self):
 
-        super().__init__(access_token=token)
-        # .access_token(token)
-        # self
+        self.genius = Genius(
+            token,
+            skip_non_songs=True,
+            excluded_terms=["(Remix)", "(Live)"],
+            remove_section_headers=True
+        )
 
-    # def __exit__(self, exc_type, exc_val, exc_tb):
-    #     if self.close:
-    #         self.quit()
-
-    def get_artist(self, artist, max_songs=3, sort='title'):
-        artist = self.search_artist(artist, max_songs=max_songs, sort=sort)
-        print(artist)
+    def get_artist(self, artist, max_songs=3, sort='popularity'):
+        genius = self.genius
+        try:
+            artist = genius.search_artist(
+                artist, max_songs=max_songs, sort=sort).songs
+            for i in artist:
+                print(i.lyrics)
+            # print(artist)
+        except:
+            raise
