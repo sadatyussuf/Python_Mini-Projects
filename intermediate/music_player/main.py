094d96c66e4a61481305229dc6c34fffbdea7c1e
sadatyussuf <tommyleespartan0@gmail.com>
2023-02-11 14:38:53 +0000

    completed the image and label widget

--- intermediate/music_player/main.py
+++ intermediate/music_player/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import pygame
 import pathlib
 import stagger
@@ -17,42 +18,77 @@ mp3 = stagger.read_tag(BASE_DIR)
 by_data = mp3[stagger.id3.APIC][0].data
 im = io.BytesIO(by_data)
 imageFile = Image.open(im)
-# print(mp3.artist)
-# print(mp3.album)
-# print(mp3.picture)
-# print(BASE_DIR)
-print(imageFile)
+
+print(mp3.artist)
+print(mp3.album)
 
 
 window = tk.Tk()
 
 window.title("Music Player")
-window.configure(background="#33393d")
+window.configure(background="#262626")
 window.geometry(f"{WIDTH}x{HEIGHT}")
-window.resizable(False, False)
+# window.resizable(False, False)
 
 
-image_frame = tk.Frame(master=window, width=250, height=250, bg="red")
-image_frame.grid(row=1, column=3, padx=20, pady=70)
+# ---------------------------------------------------------------------------------
+top_frame = tk.Frame(window, width=WIDTH, height=100, bg="#59004B")
+top_frame.pack()
 
-# image_frame.place(anchor="center", relx=0.5, rely=0.5)
-# Resize the image
-new_size = (250, 250)
+image_frame = tk.Frame(top_frame, width=100, height=100, bg="yellow")
+image_frame.pack(side="left")
+
+label_frame = tk.Frame(master=top_frame, bg="#59024B")
+label_frame.pack()
+
+artist_name = tk.Label(
+    label_frame,
+    text=f"{mp3.artist}",
+    justify=["center"],
+    font=("Helvetica", 30),
+    bg="#59024B",
+    fg="#E372F2",
+)
+album_name = tk.Label(
+    label_frame,
+    text=f"{mp3.album}",
+    justify=["center"],
+    font=("Helvetica", 15),
+    bg="#59024B",
+    fg="#E372F2",
+)
+
+artist_name.pack(ipady=5, ipadx=(WIDTH - 200) / 2)
+album_name.pack(ipady=5, ipadx=(WIDTH - 200) / 2)
+
+
+# # Resize the image
+new_size = (96, 96)
 image = imageFile.resize(new_size, Image.ANTIALIAS)
 
 img = ImageTk.PhotoImage(image)
 label = tk.Label(image_frame, image=img)
 label.pack()
+# ---------------------------------------------------------------------------------
+
+# left_frame = tk.Frame(master=window, width=WIDTH / 2, height=HEIGHT - 100, bg="#E372F2")
+# left_frame.pack(side="left")
+
+# right_frame = tk.Frame(
+#     master=window, width=WIDTH / 2, height=HEIGHT - 100, bg="#D90DA2"
+# )
+# right_frame.pack(side="right")
 
 
-p_frame = tk.Frame(master=window, width=250, height=100, bg="yellow")
-p_frame.grid(row=5, column=3, padx=20)
+# p_frame = tk.Frame(master=window, width=250, height=250, bg="yellow")
+# p_frame.grid(row=1, column=1)
 
-playlists_frame = tk.Frame(master=window, width=400, height=300, bg="green")
-playlists_frame.grid(row=1, column=5, padx=70)
 
-volumes_frame = tk.Frame(master=window, width=400, height=50, bg="blue")
-volumes_frame.grid(row=5, column=5)
+# playlists_frame = tk.Frame(master=window, width=400, height=300, bg="green")
+# playlists_frame.grid(row=1, column=5, padx=70)
+
+# volumes_frame = tk.Frame(master=window, width=400, height=50, bg="blue")
+# volumes_frame.grid(row=5, column=5)
 
 
 # frame_btn = tk.Frame(master=window, width=WIDTH, bg="#000")
