88ad814095662e3117fc7a7e3cac7d9f218e0111
sadatyussuf <tommyleespartan0@gmail.com>
2023-03-22 23:28:10 +0000

    added a func to improve formatting and displaying of context in the txt file

--- intermediate/news_updater_with_vioce/main.py
+++ intermediate/news_updater_with_vioce/main.py
@@ -16,9 +16,20 @@ def write_to_file(filename: str, news: list):
     unique_id = uuid.uuid4()
     file_name = f"{filename}_{unique_id}.txt"
 
+    def formatting_text(new):
+        title, author = new["title"], ["newauthor"]
+        url, urlToImage = new["url"], new["urlToImage"]
+        source_name, publishedAt = new["source"]["name"], new["publishedAt"]
+        content, description = new["content"], new["description"]
+
+        format_str = f"\n ---------- Author = {author} ---------- Source = {source_name} ---------- \n \n {'':^10} {title.upper()} \n \n {content} \n \n {description.lower()} \n \n URL = {url} \n UrlToImage = {urlToImage}  \n \n ---------- ---------- Date Published = {publishedAt} ---------- ---------- \n \n \n \n"
+
+        return format_str
+
+    news = list(map(formatting_text, news))
     with open(BASE_DIR.joinpath(file_name), "w") as f:
         for new in news:
-            json.dump(new, f, sort_keys=True, indent=2)
+            f.write(new)
 
 
 def get_news(token: str, query: str):
@@ -34,23 +45,18 @@ def get_news(token: str, query: str):
         language="en",
     )
 
-    # /v2/top-headlines/sources
-    # sources = newsapi.get_sources()
-
     json_str = json.dumps(everything)
     py_dict = json.loads(json_str)
 
     articles = py_dict["articles"]
-
-    # print(len(py_dict["articles"]))
 
     write_to_file(query, articles)
 
 
 def read_news():
     engine = pyttsx3.init()
-
-    rate = engine.getProperty("rate")  # getting details of current speaking rate
+    # getting details of current speaking rate
+    rate = engine.getProperty("rate")
     print(rate)  # printing current voice rate
     engine.setProperty("rate", 125)  # setting up new voice rate
 
